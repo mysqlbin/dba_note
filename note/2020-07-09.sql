da86c6df1582461817be79d3e4923c17f70e6e52
mysqlbin <1224056230@qq.com>
2020-07-13 09:36:45 +0800

    .

---
+++ note/2020-07-09.sql
@@ -0,0 +1,12 @@
+
+
+[Msg] [Exp] Table Type - Text file
+[Msg] [Exp] Getting and Exporting data ...
+[Msg] [Exp] Export query [Untitled Query]
+[Msg] [Exp] Export to - E:\work\20200709table_clubgamescorerobotdetail.csv
+[Msg] [Exp] 2013 - Lost connection to MySQL server during query
+[Msg] [Exp] SQL: select nClubID,nTableID,nChairID,szToken,nRound,nBaseScore,nPlayCount,tStartTime,tEndTime,nPlayerID,bRobot,szCardData,nEnterScore,nBetScore,nValidBet,nResultMoney,nPlayerScore,nTax,nGameType,nServerID,nCardData,nBankID,szExtChar
+from table_clubgamescorerobotdetail
+
+[Msg] [Exp] Finished - Unsuccessfully
+--------------------------------------------------
