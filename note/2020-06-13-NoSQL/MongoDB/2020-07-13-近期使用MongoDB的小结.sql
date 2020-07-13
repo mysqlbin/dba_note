2967387d9fee28d21848ea792c64fee47495993f
mysqlbin <1224056230@qq.com>
2020-07-13 18:33:51 +0800

    .

--- note/2020-06-13-NoSQL/MongoDB/2020-07-13-近期使用MongoDB的小结.sql
+++ note/2020-06-13-NoSQL/MongoDB/2020-07-13-近期使用MongoDB的小结.sql
@@ -1,4 +1,7 @@
 
+1. 当前实例的Mongodb集合已经有数据，是否可以通过别的实例备份同一个集合的数据，然后导入备份的数据到当前实例已有集合中，而不影响已经有的数据？
+2. 集合写入的数据跟导入的数据key的位置/先后顺序不一样，是否有问题？
+3. 在当前实例备份某个文档的数据，再往当前实例中导入备份，会报 ObjectID 冲突
 
 
 1. 当前实例的Mongodb集合已经有数据，是否可以通过别的实例备份同一个集合的数据，然后导入备份的数据到当前实例已有集合中，而不影响已经有的数据？
@@ -14,29 +17,11 @@
 		repl_set:PRIMARY> db.table_clubgamescorerobotdetail.findOne({"szToken": "10001-649413-1575254160-1"})
 		{
 			"_id" : ObjectId("5f07c26053b953b6b8bd5a7b"),
-			"nClubID" : 10001,
-			"nTableID" : 649413,
-			"nChairID" : 2,
-			"szToken" : "10001-649413-1575254160-1",
-			"nRound" : 1,
-			"nBaseScore" : 2000,
-			"nPlayCount" : 2,
-			"tStartTime" : "2019-12-02 10:36:00",
-			"tEndTime" : "2019-12-02 10:38:30",
-			"nPlayerID" : 93480,
-			"bRobot" : 1,
-			"szCardData" : "",
-			"nEnterScore" : 87061,
-			"nBetScore" : 2080,
-			"nValidBet" : 2080,
-			"nResultMoney" : 1976,
-			"nPlayerScore" : 89037,
-			"nTax" : 104,
-			"nGameType" : 24,
-			"nServerID" : 2401,
-			"nCardData" : "羸",
-			"nBankID" : 0,
-			"szExtChar" : ""
+			"nResultMoney" : 3800,
+			"nServerID" : 2501,
+			"nBetScore" : 4000,
+			"bRobot" : 1
+			
 		}
 
 	应用程序写入的数据
@@ -46,32 +31,14 @@
 			"nBetScore" : 4000,
 			"bRobot" : 1,
 			"nResultMoney" : 3800,
-			"nCardData" : "01261111370929362809012425",
-			"nChairID" : 3,
-			"nTableID" : 532706,
-			"nTax" : 200,
-			"CardData" : "01261111370929362809012425,06211712230213330805241113,07272223323231130507022504,08181719052716351501152823,2",
-			"szToken" : "10001-532706-1594634034-1",
-			"szCardData" : "",
-			"nRound" : 1,
-			"tEndTime" : "2020-07-13 17:56:02",
-			"nPlayCount" : 4,
-			"nEnterScore" : 94905,
-			"nClubID" : 10001,
-			"nBaseScore" : 1000,
-			"nBankID" : 0,
-			"nGameType" : 25,
-			"nPlayerScore" : 98705,
-			"tStartTime" : "2020-07-13 17:53:54",
-			"CreateTime" : ISODate("2020-07-13T09:56:02Z"),
-			"nValidBet" : 4000,
-			"nPlayerID" : 75182,
-			"szExtChar" : "",
 			"nServerID" : 2501
 		}
 
 
 	
-3. 
+3. 在当前实例备份某个文档的数据，再往当前实例中导入备份，会报 ObjectID 冲突
+	原因：ObjectID 是全局唯一的。
+
+
 
 	
