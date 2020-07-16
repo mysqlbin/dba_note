95df2f23cfeb5a695e8744707574a01ffa0d0bad
mysqlbin <1224056230@qq.com>
2020-07-16 18:43:36 +0800

    .

--- note/2020-06-29-今日读的文章.sql
+++
@@ -1,7 +0,0 @@
-
-
-https://juejin.im/post/5ede6436518825430c3acaf4  MySQL 事务   --把MySQL实战45讲相关的知识点口述出来了
-
-
-
-
---
+++ note/2020-06-29-面试相关的文章.sql
@@ -0,0 +1,10 @@
+
+
+https://juejin.im/post/5ede6436518825430c3acaf4  MySQL 事务   --把MySQL实战45讲相关的知识点口述出来了
+
+
+https://mp.weixin.qq.com/s/vkTb1E2in67JdG_Hk6SlOg  MySQL 四万字精华总结 + 面试100 问，和面试官扯皮绰绰有余  --2020-07-16
+
+	跟着题目先自己讲给自己听，看看自己会满意吗，不满意就继续学习这个点，如此反复
+
+https://www.jianshu.com/p/a59fe4fcddfb  史上最详细的一线大厂Mysql面试题详解
