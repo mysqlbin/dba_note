2eac1aea4200a41a390bef94a6d82fb291184eb2
binbin <1224056230@qq.com>
2021-06-09 16:09:05 +0800

    .

--- note/2021-06-03-MySQL 源码相关/2021-06-08-MVCC相关/2021-06-08.sql
+++ note/2021-06-03-MySQL 源码相关/2021-06-08-MVCC相关/2021-06-08.sql
@@ -1,8 +1,72 @@
 
+-1. trx_sys_t
 0. InnoDB隐藏字段源码分析
 1. read view 中主要包含4个比较重要的内容
 2. 判断可见性的源码
 3. row_search_mvcc和row_search_no_mvcc的入口函数
+4. lock_clust_rec_cons_read_sees和row_vers_build_for_consistent_read
+	4.1 row_search_mvcc->lock_clust_rec_cons_read_sees 判断可见性
+	4.2  row_vers_build_for_consistent_read构建undo历史版本
+5. 相关参考
+
+
+-1. trx_sys_t
+	trx_sys_t是整个事务的管理系统，包括MVCC的控制模块和数据库所有的活跃事务，以及回滚段 rollback segments 管理。
+	
+	/* mysql-5.7.26\storage\innobase\include\trx0sys.h */
+	struct trx_sys_t {
+
+	
+		MVCC*		mvcc;		/*!< Multi version concurrency control manager */
+		volatile trx_id_t max_trx_id;	    /* 下一个事务被分配的ID */
+						
+		trx_ut_list_t	serialisation_list; /* 所有活跃的读写事务 */
+						
+		trx_id_t	rw_max_trx_id;	 		/* 活跃事务的最大ID */ 
+		
+		trx_ut_list_t	rw_trx_list;		/* 当前系统所有的读写事务，包括活跃的和已经提交的事务。崩溃恢复后产生的事务和系统的事务也放在上面。 */   
+
+		trx_ut_list_t	mysql_trx_list;	    /* 所有用户创建的事务，系统的事务和奔溃恢复后的事务不会在这个链表上，但是这个链表上可能会有还没开始的用户事务。 */
+
+		trx_ids_t	rw_trx_ids; 			/* 活跃的事务ID列表 */
+
+	
+		trx_rseg_t*	rseg_array[TRX_SYS_N_RSEGS];
+						/*!< Pointer array to rollback
+						segments; NULL if slot not in use;
+						created and destroyed in
+						single-threaded mode; not protected
+						by any mutex, because it is read-only
+						during multi-threaded operation */
+		ulint		rseg_history_len;
+						/*!< Length of the TRX_RSEG_HISTORY
+						list (update undo logs for committed
+						transactions), protected by
+						rseg->mutex */
+
+		trx_rseg_t*	const pending_purge_rseg_array[TRX_SYS_N_RSEGS];
+						/*!< Pointer array to rollback segments
+						between slot-1..slot-srv_tmp_undo_logs
+						that are now replaced by non-redo
+						rollback segments. We need them for
+						scheduling purge if any of the rollback
+						segment has pending records to purge. */
+
+		TrxIdSet	rw_trx_set;	/*!< Mapping from transaction id
+						to transaction instance */
+
+		ulint		n_prepared_trx;	/*!< Number of transactions currently
+						in the XA PREPARED state */
+
+		ulint		n_prepared_recovered_trx; /*!< Number of transactions
+						currently in XA PREPARED state that are
+						also recovered. Such transactions cannot
+						be added during runtime. They can only
+						occur after recovery if mysqld crashed
+						while there were XA PREPARED
+						transactions. We disable query cache
+						if such transactions exist. */
+	};
 
 
 
@@ -84,7 +148,7 @@
 	}
 
 
-	/*mysql-5.7.26\storage\innobase\include\read0types.h*/
+	/* mysql-5.7.26\storage\innobase\include\read0types.h */
 	
 	private:
 		/** The read should not see any transaction with trx id >= this
@@ -124,10 +188,8 @@
 
 	/* storage/innobase/include/read0types.h */
 
-	/** Check whether the changes by id are visible.
-	@param[in]	id	transaction id to check against the view
-	@param[in]	name	table name
-	@return whether the view sees the modifications of id. */
+	/* changes_visible()的返回结果true代表可见，false代表不可见. */
+	
 	bool changes_visible(
 		trx_id_t		id,
 		const table_name_t&	name) const
@@ -165,27 +227,9 @@
 
 3. row_search_mvcc和row_search_no_mvcc的入口函数
 
-	/** Searches for rows in the database. This is used in the interface to
-	MySQL. This function opens a cursor, and also implements fetch next
-	and fetch prev. NOTE that if we do a search with a full key value
-	from a unique index (ROW_SEL_EXACT), then we will not store the cursor
-	position and fetch next or fetch prev must not be tried to the cursor!
+	/* mysql-5.7.26\storage\innobase\include\row0sel.ic */
+	
 
-	@param[out]	buf		buffer for the fetched row in MySQL format
-	@param[in]	mode		search mode PAGE_CUR_L
-	@param[in,out]	prebuilt	prebuilt struct for the table handler;
-					this contains the info to search_tuple,
-					index; if search tuple contains 0 field then
-					we position the cursor at start or the end of
-					index, depending on 'mode'
-	@param[in]	match_mode	0 or ROW_SEL_EXACT or ROW_SEL_EXACT_PREFIX
-	@param[in]	direction	0 or ROW_SEL_NEXT or ROW_SEL_PREV;
-					Note: if this is != 0, then prebuilt must has a
-					pcur with stored position! In opening of a
-					cursor 'direction' should be 0.
-	@return DB_SUCCESS, DB_RECORD_NOT_FOUND, DB_END_OF_INDEX, DB_DEADLOCK,
-	DB_LOCK_TABLE_FULL, DB_CORRUPTION, or DB_TOO_BIG_RECORD */
-	UNIV_INLINE
 	dberr_t
 	row_search_for_mysql(
 		byte*			buf,
@@ -202,6 +246,184 @@
 				buf, mode, prebuilt, match_mode, direction));
 		}
 	}
+	
+	dberr_t
+	row_search_mvcc(
+		byte*		buf,
+		page_cur_mode_t	mode,
+		row_prebuilt_t*	prebuilt,
+		ulint		match_mode,
+		ulint		direction)
+	{
+		DBUG_ENTER("row_search_mvcc");
 
 
 
+
+
+4. lock_clust_rec_cons_read_sees和row_vers_build_for_consistent_read
+
+	当用mvcc读取的时候（row_search_mvcc），对于聚簇索引，当拿到一条记录后，会先通过函数 lock_clust_rec_cons_read_sees 判断可见性，如果不可见会再构建老版本数据 row_vers_build_for_consistent_read 。
+
+
+	4.1 row_search_mvcc->lock_clust_rec_cons_read_sees 判断可见性
+
+		/* mysql-5.7.26\storage\innobase\row\row0sel.cc */
+		
+		/*********************************************************************//**
+		Checks that a record is seen in a consistent read.
+		@return true if sees, or false if an earlier version of the record
+		should be retrieved */
+		bool
+		lock_clust_rec_cons_read_sees(
+		/*==========================*/
+			const rec_t*	rec,	/*!< in: user record which should be read or
+						passed over by a read cursor */
+			dict_index_t*	index,	/*!< in: clustered index */
+			const ulint*	offsets,/*!< in: rec_get_offsets(rec, index) */
+			ReadView*	view)	/*!< in: consistent read view */
+		{
+			ut_ad(dict_index_is_clust(index));
+			ut_ad(page_rec_is_user_rec(rec));
+			ut_ad(rec_offs_validate(rec, index, offsets));
+
+			/* Temp-tables are not shared across connections and multiple
+			transactions from different connections cannot simultaneously
+			operate on same temp-table and so read of temp-table is
+			always consistent read. */
+			
+			/*
+				临时表不跨连接共享，来自不同连接的多个事务不能同时对同一个临时表进行操作，因此临时表的读取始终是一致读取   -- 临时表是session也就是线程级别的，因此临时表的读取始终是一致性读取
+				srv_read_only_mode： 此处获取事务当前是否是只读属性，可以看到他和我们的read_only参数设置事物是ddl事物是否是内部事物有关
+			*/
+			if (srv_read_only_mode || dict_table_is_temporary(index->table)) {
+				ut_ad(view == 0 || dict_table_is_temporary(index->table));
+				return(true);
+			}
+
+			/* NOTE that we call this function while holding the search
+			system latch. */
+			
+			/* 获取该条 Record 的trx_id */
+			trx_id_t	trx_id = row_get_rec_trx_id(rec, index, offsets);
+			
+			/* 判断记录可见性 */
+			return(view->changes_visible(trx_id, index->table->name));
+		} 
+		
+		
+	
+	4.2  row_vers_build_for_consistent_read 构建undo历史版本
+	
+		/* mysql-5.7.26\storage\innobase\row\row0vers.cc */
+
+		/*****************************************************************//**
+		Constructs the version of a clustered index record which a consistent
+		read should see. We assume that the trx id stored in rec is such that
+		the consistent read should not see rec in its present version.
+		@return DB_SUCCESS or DB_MISSING_HISTORY */
+		dberr_t
+		row_vers_build_for_consistent_read()
+		{
+		
+			/* If purge can't see the record then we can't rely on
+			the UNDO log record. */
+			
+			/* 如果purge 看不到记录，那么我们就不能依赖UNDO 日志记录。 */
+			
+				for (;;) {
+					mem_heap_t*	prev_heap = heap;
+
+					heap = mem_heap_create(1024);
+
+					if (vrow) {
+						*vrow = NULL;
+					}
+
+					/* If purge can't see the record then we can't rely on
+					the UNDO log record. */
+					/* 构建 undo 版本 */
+					bool	purge_sees = trx_undo_prev_version_build(
+						rec, mtr, version, index, *offsets, heap,
+						&prev_version, NULL, vrow, 0);
+
+					err  = (purge_sees) ? DB_SUCCESS : DB_MISSING_HISTORY;
+					
+			}
+
+		}
+		-----------------------------------------------------------------------------------------
+		
+		/* 构建聚集索引记录的undo版本 */
+		/* mysql-5.7.26\storage\innobase\trx\trx0rec.cc */
+		
+		/*******************************************************************//**
+		Build a previous version of a clustered index record. The caller must
+		hold a latch on the index page of the clustered index record.
+		@retval true if previous version was built, or if it was an insert
+		or the table has been rebuilt
+		@retval false if the previous version is earlier than purge_view,
+		or being purged, which means that it may have been removed */
+		bool
+		trx_undo_prev_version_build()
+	
+		
+	
+		/*============== BUILDING PREVIOUS VERSION OF A RECORD(获取对应的 Undo Record 内容) ===============*/
+		/******************************************************************//**
+		Copies an undo record to heap. This function can be called if we know that
+		the undo log record exists.
+		@return own: copy of the record */
+		trx_undo_rec_t*
+		trx_undo_get_undo_rec_low(
+		/*======================*/
+			roll_ptr_t	roll_ptr,	/*!< in: roll pointer to record */
+			mem_heap_t*	heap,		/*!< in: memory heap where copied */
+			bool		is_redo_rseg)	/*!< in: true if redo rseg. */
+		{
+
+
+			/* 解析 roll_ptr 指针内容. */
+			trx_undo_decode_roll_ptr(roll_ptr, &is_insert, &rseg_id, &page_no,
+						 &offset);
+			/* 根据回滚段 ID 查找回滚段。*/
+			rseg = trx_rseg_get_on_id(rseg_id, is_redo_rseg);
+
+			mtr_start(&mtr);
+			
+			/* 在回滚段中获取对应的 Undo Page. */
+			undo_page = trx_undo_page_get_s_latched(
+				page_id_t(rseg->space, page_no), rseg->page_size,
+				&mtr);
+				
+			/* 通过 offset 获取对应的 Undo Record. */
+			undo_rec = trx_undo_rec_copy(undo_page + offset, heap);
+
+			mtr_commit(&mtr);
+
+			return(undo_rec);
+		}
+
+
+5. 构建undo历史版本的流程
+
+	1. 构建老版本记录（trx_undo_prev_version_build）需要持有page latch，因此如果Undo链太长的话，其他请求该page的线程可能等待时间过长导致crash 
+	
+	2. 在构建老版本的过程中，总是需要创建heap来存储旧版本记录，实际上这个heap是可以重用的，无需总是重复构建
+	3. 如果回滚段类型是INSERT，就完全没有必要去看Undo日志了，因为一个未提交事务的新插入记录，对其他事务而言总是不可见的。
+	4. 对于聚集索引我们知道其记录中存有修改该记录的事务id，我们可以直接判断是否需要构建老版本(lock_clust_rec_cons_read_sees)
+		但对于二级索引记录，并未存储事务id，而是每次更新记录时，同时更新记录所在的page上的事务id（PAGE_MAX_TRX_ID），
+		如果该事务id对当前事务是可见的，那么就无需去构建老版本了，否则就需要去回表查询对应的聚集索引记录，然后判断可见性（lock_sec_rec_cons_read_sees）
+
+
+6. 相关参考
+
+	http://mysql.taobao.org/monthly/2015/04/01/
+	http://mysql.taobao.org/monthly/2018/11/04/
+	https://my.oschina.net/alchemystar/blog/1927425
+    https://dev.mysql.com/doc/dev/mysql-server/8.0.12/classReadView.html
+   
+    https://blog.csdn.net/maray/article/details/80848544
+	
+	https://www.leviathan.vip/2019/03/20/InnoDB%E7%9A%84%E4%BA%8B%E5%8A%A1%E5%88%86%E6%9E%90-MVCC/   
+	
