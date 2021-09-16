fbd5e30f2abb5447eacce63ba34092ca192fa317
mysqlbin <13202095158@163.com>
2021-09-16 23:50:22 +0800

    .

--- note/2020-05-28-投稿相关/2020-05-28-日常投稿文章.sql
+++ note/2020-05-28-投稿相关/2020-05-28-日常投稿文章.sql
@@ -3,3 +3,4 @@
 https://mp.weixin.qq.com/s/oWXr68fBfWJcxKLL4V_IUw  一次死锁的过程分析
 https://mp.weixin.qq.com/s/W05GFKSoa2R1wFgkVLd4Dg  update SQL在MySQL的内部生命历程是怎样的
 
+https://mp.weixin.qq.com/s/mJ6a-sV2C2ru5pA5QNAAZQ	pt-osc在线重建表导致死锁的分析及对应的优化方案
