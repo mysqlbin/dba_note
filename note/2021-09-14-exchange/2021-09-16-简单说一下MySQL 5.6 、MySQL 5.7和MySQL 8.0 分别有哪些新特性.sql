6f85b9a7cb4cbc7eeb2593aa2f64e0612277743c
binbin <1224056230@qq.com>
2021-09-16 11:32:34 +0800

    .

---
+++ note/2021-09-14-exchange/2021-09-16-简单说一下MySQL 5.6 、MySQL 5.7和MySQL 8.0 分别有哪些新特性.sql
@@ -0,0 +1,34 @@
+
+
+5.6 
+	复制方面
+		支持GTID复制
+		半同步复制
+		延迟复制 
+		
+    支持Online DDL 
+    支持BP缓冲池的预热功能，数据库重启后，BP缓冲池的热点数据还存在，5.7版本是默认开启的
+    
+5.7 
+	复制方面
+	
+		组复制，也就是MGR
+		增强半同步复制
+		基于行的并行复制 
+		
+    在线修改BP缓冲池大小
+        使用不当，会造成DML阻塞。
+        遇到过在线收缩BP缓冲池空间，导致阻塞业务的DML请求。
+		
+    支持json字段
+    
+
+8.0
+
+    快速加列(什么时候也能支持快速加索引就好了)、hash join 
+    支持在线关闭和开启Redo Log，加快逻辑备份的导入。
+        ALTER INSTANCE ENABLE INNODB REDO_LOG; 
+        ALTER INSTANCE DISABLE INNODB REDO_LOG;
+	优化了唯一索引的1个加锁BUG
+	
+https://www.cnblogs.com/ivictor/p/9807284.html   MySQL 5.6, 5.7, 8.0的新特性
