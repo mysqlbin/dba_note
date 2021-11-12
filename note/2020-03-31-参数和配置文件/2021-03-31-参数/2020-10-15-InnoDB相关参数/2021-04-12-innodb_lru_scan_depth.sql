b330b9b77df227cfbaa9e6868ca17e697eb3028e
binbin <1224056230@qq.com>
2021-11-12 14:59:01 +0800

    .

--- note/2020-03-31-参数和配置文件/2021-03-31-参数/2020-10-15-InnoDB相关参数/2021-04-12-innodb_lru_scan_depth.sql
+++ note/2020-03-31-参数和配置文件/2021-03-31-参数/2020-10-15-InnoDB相关参数/2021-04-12-innodb_lru_scan_depth.sql
@@ -1,10 +1,21 @@
 
-控制LRU列表中可用页的数量, 该值默认为1024 
-
-当系统的IO比较空闲的时候，可以适当将这个参数设大，当IO吃紧时，需要适当减小
-	-- 有待验证。
+innodb_lru_scan_depth 是用来控制LRU刷脏的，有2个含义：
 	
-参考
+	默认值为 1024 
+	
+	innodb_lru_scan_depth 表示 innodb lru列表扫描的深度
+	
+	当Buffer Pool的Free list的 Page数量小于innodb_lru_scan_depth时，才进行LRU刷脏
+
+	每次LRU刷脏，扫描的页数不超过 innodb_lru_scan_depth 个。
+		
+	 
+相关参考
+	
+	https://dev.mysql.com/doc/refman/5.7/en/innodb-parameters.html#sysvar_innodb_lru_scan_depth
+	 
+	https://mp.weixin.qq.com/s/i0sIfUqUUX5c_GkFTYh64Q   
+	
 	https://www.cnblogs.com/zengkefu/p/5692803.html
 
 	《2020-10-27-InnoDB buffer pool缓冲池的重新学习和整理》
