dffa536b9384570e1a68b82a0d6f6c5a058e4c9d
binbin <1224056230@qq.com>
2021-11-25 11:45:23 +0800

    .

--- note/2021-06-03-MySQL 源码相关/2020-02-27-语句的执行流程/2021-11-22 - analyze table/2021-11-23 - 请求重新计算统计信息的流程分析.sql
+++ note/2021-06-03-MySQL 源码相关/2020-02-27-语句的执行流程/2021-11-22 - analyze table/2021-11-23 - 请求重新计算统计信息的流程分析.sql
@@ -4,6 +4,7 @@
 3. 请求重新计算统计信息的流程
 4. 参考源码级相关文章
 5. 其它待解决
+6. 小结
 
 
 1. 前言
@@ -24,10 +25,8 @@
 	
 	2. 进入到自动重新计算后台线程，持久化统计信息在以下情况会被自动更新(先把表的ID加入到 recalc_pool 中)：
 			
-		1. INNODB_STATS_AUTO_RECALC=ON 情况下，只要满足下面的(1)或者(2)的其中1个条件就会统计到修改表的行数中，当表中10%的数据被修改 ，就会把表的ID加入到 recalc_pool 中：
-			(1). 通过 delete 删除数据
-			(2). 通过 update 更新并且有更新二级索引的数据
-			-- 所以，数据表只有insert的场景，是不会加入到修改表的行数中的。
+		1. INNODB_STATS_AUTO_RECALC=ON 情况下，执行DML语句后，当表中10%的数据被修改 ，就会把表的ID加入到 recalc_pool 中：
+			其中，update语句如果只更新到非索引列，那就不会影响统计信息
 			
 		2. 添加/删除字段、添加索引。  
 			
@@ -59,10 +58,8 @@
 		1.1 所有需要被重新计算的表会加入到recalc_pool中，recalc_pool初始化大小为128，随后如果需要再被扩大。
 		
 		1.2 在做完DML后，会调用函数 row_update_for_mysql_using_upd_graph 判断，只要满足下面的其中1个条件就会直接进入 row_update_statistics_if_needed 函数判断是否需要更新统计信息 ：
-
-			1. 只会在DELETE或者UPDATE有更改到索引列，如果只更新到非索引列，那就不会影响统计信息。
-			2. delete肯定会影响到所有列
-			
+			其中，update语句如果只更新到非索引列，那就不会影响统计信息
+		
 				
 		1.3 row_update_statistics_if_needed 函数
 			
@@ -99,6 +96,7 @@
 
 
 4. 参考源码级相关文章
+	
 	https://developer.aliyun.com/article/41045
 	https://tusundong.top/post/empty_cardinality_bug.html	
 	http://mysql.taobao.org/monthly/2020/03/08/
@@ -107,5 +105,71 @@
 5. 其它待解决
 
 	1. dict_stats_thread 后台线程多久运行一次？
+	
 	2. 添加/删除字段、添加索引 需要把表ID 加入到 recalc_pool 吗？
 	
+	3. 获取表的总行数，应该是从information_schmea.tables获取吗
+		n_rows = dict_table_get_n_rows(table);
+
+
+6. 小结
+	
+	主库的统计信息相对比较可靠。
+		
+	从库的不可靠，特别是大表，只有 insert 的场景。有读写分离的要注意了，要定期更新从库核心表的统计信息，从而避免因为统计信息不够新，导致SQL选择了错误的索引引发SQL执行速度慢。
+	
+	如果表有500W行记录，纯 insert 场景，大概要再插入55W行记录 才会触发自动更新统计信息，如果该表每天插入1W行记录，那么大概需要55天才会触发自动更新统计信息
+		因此，统计信息相对于当前时间，有55天的落后。
+	
+		mysql> select 550000/5500000;
+		+----------------+
+		| 550000/5500000 |
+		+----------------+
+		|         0.1000 |
+		+----------------+
+		1 row in set (0.00 sec)
+	
+	按日期分表的场景，也就是每天1个表。
+	
+		slave
+			mysql> select * from mysql.innodb_table_stats  where database_name='niuniuh5_db' order by last_update desc limit 15;
+			+---------------+-----------------------------------+---------------------+--------+----------------------+--------------------------+
+			| database_name | table_name                        | last_update         | n_rows | clustered_index_size | sum_of_other_index_sizes |
+			+---------------+-----------------------------------+---------------------+--------+----------------------+--------------------------+
+			| niuniuh5_db   | table_user_gamelock               | 2021-11-24 16:39:15 |     43 |                    1 |                        1 |
+			| niuniuh5_db   | table_clubgamescoredetail20211124 | 2021-11-24 16:37:01 |  56250 |                 1315 |                     1158 |
+			| niuniuh5_db   | table_clublogscore20211124        | 2021-11-24 16:24:01 |  73116 |                  545 |                      708 |
+			| niuniuh5_db   | table_third_score_detail20211124  | 2021-11-24 16:23:40 |  53644 |                  931 |                      450 |
+			| niuniuh5_db   | table_webdata_excelexport         | 2021-11-24 13:34:49 |     11 |                    1 |                        2 |
+			| niuniuh5_db   | table_third_score_detail20210824  | 2021-11-24 08:30:52 |      0 |                    1 |                        2 |
+			
+			-- table_clublogscore20210824 表有删除全表的数据。
+			| niuniuh5_db   | table_clublogscore20210824        | 2021-11-24 08:26:06 |      0 |                    1 |                        4 |
+			| niuniuh5_db   | table_clubgamescoredetail20210824 | 2021-11-24 08:20:45 |      0 |                    1 |                        6 |
+			
+			| niuniuh5_db   | table_third_score_detail20211123  | 2021-11-23 23:59:43 |  98154 |                 1764 |                      772 |
+			| niuniuh5_db   | table_clublogscore20211123        | 2021-11-23 22:56:31 | 118714 |                  931 |                     1092 |
+			| niuniuh5_db   | table_clubgamescoredetail20211123 | 2021-11-23 22:30:13 |  80136 |                 2277 |                     1736 |
+			
+			| niuniuh5_db   | table_third_score_detail20210823  | 2021-11-23 08:30:38 |      0 |                    1 |                        2 |
+			
+			-- table_clublogscore20210823 表有删除全表的数据。
+			| niuniuh5_db   | table_clublogscore20210823        | 2021-11-23 08:25:51 |      0 |                    1 |                        4 |
+			| niuniuh5_db   | table_clubgamescoredetail20210823 | 2021-11-23 08:20:39 |      0 |                    1 |                        6 |
+			| niuniuh5_db   | table_third_score_detail20211122  | 2021-11-22 23:57:33 | 105227 |                 1892 |                      772 |
+			+---------------+-----------------------------------+---------------------+--------+----------------------+--------------------------+
+			15 rows in set (0.00 sec)
+		
+	------------------------------------------------------------------------------------------------------------------------------------
+	表的数据量大，纯insert场景，因此需要插入记录数相对较多，才会触发自动更新统计信息，因此统计信息相对于当前时间，有一定的落后。
+	表的数据量小，纯insert场景，不需要插入记录数太多，就会触发自动更新统计信息，因此，统计信息相对较新。
+	这里要总结下。
+	------------------------------------------------------------------------------------------------------------------------------------
+	
+	什么是统计信息？ 
+		就是表中各个索引的基数，MySQL 优化器选择索引依赖于索引统计信息。
+		
+		
+		
+		
+		
---
+++ note/2021-06-03-MySQL 源码相关/2020-02-27-语句的执行流程/2021-11-22 - analyze table/2021-11-25 - 从库统计信息不可靠的1个案例.sql
@@ -0,0 +1,107 @@
+
+1. 表结构和业务场景
+
+	CREATE TABLE `table_third_order` (
+	  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+	  .......................................
+	  `nStatus` int(10) NOT NULL COMMENT '订单: 0-处理中,1-成功,2-失败',
+	  .......................................
+	  PRIMARY KEY (`ID`),
+	  KEY `idx_nStatus_CreateTime` (`nStatus`,`CreateTime`),
+
+	) ENGINE=InnoDB AUTO_INCREMENT=12176015 DEFAULT CHARSET=utf8mb4 COMMENT='第三方平台兑换订单表';
+
+
+
+
+
+	数据写入订单表中，nStatus=0：
+		INSERT INTO `table_third_order`(..............., `nStatus`, ...............) VALUES (..............., 0, ...............);
+		
+		
+	有多种途径把 table_third_order.szOrder 改为 1（更新语句有更新二级索引的数据，会统计到修改表行数的计数器中），同时加上表还有insert操作，因此修改表行数超过总行数的 10%，会自动更新统计信息，但是在从库并没有自动更新统计信息。
+	
+	这个表没有删除过数据。
+
+
+2. 当前时间
+   
+	mysql> select now();
+	+---------------------+
+	| now()               |
+	+---------------------+
+	| 2021-11-25 10:02:53 |
+	+---------------------+
+	1 row in set (0.00 sec)
+
+  
+  
+  
+3. 表在主库更新统计信息的最新时间
+
+	mysql> select * from mysql.innodb_table_stats  where database_name='niuniuh5_db' and table_name="table_third_order";
+	+---------------+-------------------+---------------------+----------+----------------------+--------------------------+
+	| database_name | table_name        | last_update         | n_rows   | clustered_index_size | sum_of_other_index_sizes |
+	+---------------+-------------------+---------------------+----------+----------------------+--------------------------+
+	| niuniuh5_db   | table_third_order | 2021-11-14 17:20:39 | 11967345 |                89599 |                    96024 |
+	+---------------+-------------------+---------------------+----------+----------------------+--------------------------+
+	1 row in set (0.00 sec)
+
+	
+	select count(*) from niuniuh5_db.table_third_order;
+	select count(*) from niuniuh5_db.table_third_order where CreateTime>='2021-11-14 17:20:39';
+
+	
+	mysql> select count(*) from niuniuh5_db.table_third_order;
+	+----------+
+	| count(*) |
+	+----------+
+	| 12176022 |
+	+----------+
+	1 row in set (2.95 sec)
+
+	mysql> select count(*) from niuniuh5_db.table_third_order where CreateTime>='2021-11-14 17:20:39';
+	+----------+
+	| count(*) |
+	+----------+
+	|    94092 |
+	+----------+
+	1 row in set (0.06 sec)
+
+
+4. 表在从库更新统计信息的最新时间
+	
+	mysql> select * from mysql.innodb_table_stats  where database_name='niuniuh5_db' and table_name="table_third_order";
+	+---------------+-------------------+---------------------+---------+----------------------+--------------------------+
+	| database_name | table_name        | last_update         | n_rows  | clustered_index_size | sum_of_other_index_sizes |
+	+---------------+-------------------+---------------------+---------+----------------------+--------------------------+
+	| niuniuh5_db   | table_third_order | 2020-07-28 14:15:14 | 6832791 |                53823 |                    49590 |
+	+---------------+-------------------+---------------------+---------+----------------------+--------------------------+
+	1 row in set (0.00 sec)
+	
+	select count(*) from niuniuh5_db.table_third_order;
+	select count(*) from niuniuh5_db.table_third_order where CreateTime>='2020-07-28 14:15:14';
+	
+	mysql> select count(*) from niuniuh5_db.table_third_order;
+
+	+----------+
+	| count(*) |
+	+----------+
+	| 12176487 |
+	+----------+
+	1 row in set (1.95 sec)
+
+	mysql> select count(*) from niuniuh5_db.table_third_order where CreateTime>='2020-07-28 14:15:14';
+	+----------+
+	| count(*) |
+	+----------+
+	|  5321658 |
+	+----------+
+	1 row in set (3.03 sec)
+
+
+	可以看到，表修改的行数，达到总表行数的40%+，也没有自动更新统计信息。(从库的案例。)
+
+
+
+
