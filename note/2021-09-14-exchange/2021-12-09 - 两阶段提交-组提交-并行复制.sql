32bfe6f736169a4b8b8664615641e882d8bb3571
binbin <1224056230@qq.com>
2021-12-09 18:24:51 +0800

    .

--- note/2021-09-14-exchange/2021-12-09 - 两阶段提交-组提交-并行复制.sql
+++ note/2021-09-14-exchange/2021-12-09 - 两阶段提交-组提交-并行复制.sql
@@ -3,6 +3,8 @@
 两阶段提交
 	binlog 是Server层的， Redo log是InnoDB存储引擎层的， 是两个互不想干的逻辑，为了让它们保持逻辑上的一致，因此需要两阶段提交，把Redo log的写入操作拆分成 redo log prepare 和 commit 这2个阶段。
 	先写redo log 后写 binlog，或者先写 binlog 后写 redo log，都会存在问题。
+		-- 结合半同步/增强半同步场景，在宕机后HA发生切换，导致数据丢失。
+		
 
 
 组提交发生在哪个阶段
