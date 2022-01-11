93d7721fec2c820ac63b49cda3f656920f4cdfdd
mysqlbin <13202095158@163.com>
2022-01-11 23:13:49 +0800

    .

---
+++ note/2020-03-31-参数和配置文件/2021-03-31-参数/2020-10-15-InnoDB相关参数/2022-01-11-innodb_checksums.sql
@@ -0,0 +1,4 @@
+
+
+https://dev.mysql.com/doc/refman/5.7/en/innodb-parameters.html#sysvar_innodb_checksums
+https://dev.mysql.com/doc/refman/5.7/en/innodb-parameters.html#sysvar_innodb_checksum_algorithm
