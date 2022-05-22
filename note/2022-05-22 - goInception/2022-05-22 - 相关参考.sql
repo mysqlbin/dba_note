b655713cf6f7e2edbf2432e610745d9e4e177fe2
mysqlbin <13202095158@163.com>
2022-05-23 00:06:45 +0800

    .

--- note/2022-05-22 - goInception/2022-05-22 - 相关参考.sql
+++ note/2022-05-22 - goInception/2022-05-22 - 相关参考.sql
@@ -1,5 +1,9 @@
 
 
+https://hanchuanchuan.github.io/goInception/zh/  goInception使用文档(中文文档)
+https://hanchuanchuan.github.io/goInception/zh/rules.html	审核规则
+
+
 https://www.136.la/jingpin/show-181328.html  goinception审核规则列表
 https://blog.csdn.net/qq_42979842/article/details/118189515   goinception审核等级
 
