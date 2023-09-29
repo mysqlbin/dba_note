1d3a60b856ce59f2f33d739768efb48f6df91f05
mysqlbin <13202095158@163.com>
2023-09-29 18:03:33 +0800

    .

--- note/2021-06-03-MySQL 源码相关/2020-02-27-语句的执行流程/2023-09-24 - update的执行流程 - 口述版.sql
+++ note/2021-06-03-MySQL 源码相关/2020-02-27-语句的执行流程/2023-09-24 - update的执行流程 - 口述版.sql
@@ -1,9 +1,24 @@
 
 
 一、Server层
-
+	
 二、InnoDB存储引擎层
 
 	1. 执行阶段
-		
+		数据页加载到缓冲池
+		加排他锁
+			两阶段加锁协议
+		写日志：
+			undo
+			redo
+			binlog
+		修改值
+			
 	2. commit阶段
+		
+		两阶段提交协议
+			redo、binlog刷盘
+			
+		释放锁
+			
+		
